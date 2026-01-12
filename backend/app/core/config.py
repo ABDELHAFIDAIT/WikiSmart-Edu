6343541da868764fcb78b3958bac7e9a362c8eb0
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2026-01-12 12:22:03 +0100

    feat/ajouter le fichier config.py qui est le point d'entrée unique pour lire le fichier .env

---
+++ backend/app/core/config.py
@@ -0,0 +1,35 @@
+import os
+from pydantic import EmailStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings) :
+    POSTGRES_USER : str
+    POSTGRES_PASSWORD : str
+    POSTGRES_SERVER : str
+    POSTGRES_PORT : int
+    POSTGRES_DB : str
+    
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    
+    GROQ_API_KEY: str
+    GEMINI_API_KEY: str
+    
+    LOG_LEVEL: str = "INFO"
+    API_V1_STR: str = "/api/v1"
+    
+    @property
+    def SQLALCHEMY_DATABASE_URL(self) :
+        return(f"postgres://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}")
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=True,
+        extra="ignore"
+    )
+
+
+settings = Settings()
