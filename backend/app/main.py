5b21057fbf6e5a13126b48455d48053751172f8e
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2026-01-12 21:15:25 +0100

    feat/initialisation de l'application FastAPI et inclusion du routeur d'authentification

---
+++ backend/app/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.core.config import settings
+from app.api.v1 import auth
+
+
+app = FastAPI(
+    title="WikiSmart Edu API",
+    description="API pour l'application éducative WikiSmart (Traduction, génération de quiz et résumés via IA)",
+    version="1.0.0",
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credebtials=True,
+    allow_methods=["*"],
+    allow_headers=["*"] 
+)
+
+app.include_router(auth.router, prefix=f"{settings.API_V1_STR}/auth", tags=["Authentication"])
+
+@app.get("/")
+def root() :
+    return {
+        "message": "Bienvenue sur l'API WikiSmart Edu !",
+        "status": "online",
+        "docs_url": "/docs"
+    }
