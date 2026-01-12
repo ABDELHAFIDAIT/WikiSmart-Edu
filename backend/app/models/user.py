7d158eb09fb263b045c0d05a52a1d16cae52abdb
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2026-01-12 14:33:31 +0100

    feat/créer le modèle User

---
+++ backend/app/models/user.py
@@ -0,0 +1,15 @@
+from app.core.database import Base
+from sqlalchemy import Column, Integer, String, Boolean, Enum
+from app.models.enums import Role
+
+class User(Base) :
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False, unique=True, index=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    hashed_password = Column(String, nullable=False)
+    
+    role = Column(Enum(Role), default=Role.USER)
+    
+    is_active = Column(Boolean, default=True)
